8a30fe5b391b272d829d4a6fdb39e9f6589d1e83
jyanikhushal <khushaljyani05@gmail.com>
2026-04-01 14:28:28 +0530

    fix: standalone output for vercel

--- shreeji-frontend/next.config.ts
+++ shreeji-frontend/next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
