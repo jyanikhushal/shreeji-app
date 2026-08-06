16a00f21b0051a38b4ba9de75d27f1d8f6cee075
jyanikhushal <khushaljyani05@gmail.com>
2026-08-06 17:53:58 +0530

    Add preorder backend module with guest identity and notification integration2

--- shreeji-backend/modules/preorder/preorderGuestRoutes.js
+++ shreeji-backend/modules/preorder/preorderGuestRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { guestLogin, updateGuestName } = require('./preorderGuestController');const { guestLogin, updateGuestName, grantGuestNotificationPermission } = require('./preorderGuestController');
+const { guestLogin, updateGuestName, grantGuestNotificationPermission } = require('./preorderGuestController');
+
 router.post('/login', guestLogin);
 router.patch('/name', updateGuestName);
 router.patch('/notification-permission', grantGuestNotificationPermission);
+
 module.exports = router;
